e7550f8e300dc4f1928172597dde947005135010
Michael Oesterle <michaeloesterle01@gmail.com>
2023-08-09 15:02:14 +0200

    Create traffic/v3

---
+++ src/traffic/v3/environment.py
@@ -0,0 +1,429 @@
+import random
+from collections import Counter
+
+import networkx as nx
+import numpy as np
+import pandas as pd
+from networkx import get_edge_attributes
+from tqdm import trange
+
+
+class Car:
+    def __init__(
+        self,
+        id,
+        source,
+        target,
+        speed=0,
+        position=None,
+        *,
+        anticipation_strategy='route',
+        created_at_step=0,
+        verbose=False,
+    ) -> None:
+        assert anticipation_strategy in ['edge', 'route', 'edge_tolls', 'route_tolls'], 'Unknown anticipation strategy'
+
+        self.id = id
+        self.source = source
+        self.target = target
+        self.speed = speed
+        self.created_at_step = created_at_step
+        self.position = position if position is not None else ((source, source), 1.0)
+        self.anticipation_strategy = anticipation_strategy
+        self.verbose = verbose
+
+    def __repr__(self) -> str:
+        (v, w), p = self.position
+        return f"<Car {self.id} ({self.source} -> {self.target}) at {p} of {(v, w)}>"
+
+    def act(self, network):
+        current_node = self.position[0][1]
+        if current_node == self.target or not nx.has_path(
+            network, current_node, self.target
+        ):
+            return current_node
+        else:
+            if self.anticipation_strategy == 'route':
+                # Find the shortest path based on anticipated latencies
+                choice = random.choice(
+                    list(
+                        nx.all_shortest_paths(
+                            network,
+                            current_node,
+                            self.target,
+                            weight="anticipated_latency",
+                        )
+                    )
+                )
+
+                if self.verbose:
+                    print(f'Latencies: {get_edge_attributes(network, "anticipated_latency")}.')
+            elif self.anticipation_strategy == 'route_tolls':
+                # Find the shortest path based on the anticipated total cost (including tolls)
+                choice = random.choice(
+                    list(
+                        nx.all_shortest_paths(
+                            network,
+                            current_node,
+                            self.target,
+                            weight="anticipated_total_cost",
+                        )
+                    )
+                )
+
+                if self.verbose:
+                    print(f'Costs: {get_edge_attributes(network, "anticipated_total_cost")}.')
+            elif self.anticipation_strategy == 'edge':
+                # Find the shortest path based on the actual latencies with only the next edge being anticipated
+                final_latencies = {
+                    (v, w): attr['anticipated_latency'] if v == self.position[0][1] else attr['latency']
+                    for v, w, attr in network.edges(data=True)
+                }
+
+                if self.verbose:
+                    print(f'Latencies: {final_latencies}.')
+
+                choice = random.choice(
+                    list(
+                        nx.all_shortest_paths(
+                            network,
+                            current_node,
+                            self.target,
+                            weight=lambda v, w, _: final_latencies[(v, w)],
+                        )
+                    )
+                )
+            else:
+                # Find the shortest path based on the actual total costs with only the next edge being anticipated
+                final_cost = {
+                    (v, w): attr['anticipated_total_cost'] if v == self.position[0][1] else attr['total_cost']
+                    for v, w, attr in network.edges(data=True)
+                }
+
+                if self.verbose:
+                    print(f'Costs: {final_cost}.')
+
+                choice = random.choice(
+                    list(
+                        nx.all_shortest_paths(
+                            network,
+                            current_node,
+                            self.target,
+                            weight=lambda v, w, _: final_cost[(v, w)],
+                        )
+                    )
+                )
+            self.speed = 1 / (
+                network.edges[(choice[0], choice[1])]["anticipated_latency"]
+            )
+
+            if self.verbose:
+                print(f"Car {self.id} at {current_node} chooses {choice[1]}.")
+
+            return choice
+
+    def reset(self, source, step):
+        self.source = source
+        self.created_at_step = step
+
+        self.position = (source, source), 1.0
+        self.speed = 0
+
+
+class TrafficModel:
+    def __init__(
+        self,
+        network,
+        cars,
+        *,
+        beta=0.5,
+        R=0.1,
+        verbose=False
+    ) -> None:
+        self.network = network
+        self.cars = cars
+        self.routes = {car_id: [] for car_id in self.cars}
+        self.beta = beta
+        self.R = R
+        self.verbose = verbose
+
+        self.step_statistics = []
+        self.car_statistics = []
+        self.current_step = 0
+        self._type = "undefined"
+
+    @property
+    def allowed_network(self):
+        return nx.restricted_view(
+            self.network,
+            [],
+            [
+                (v, w)
+                for v, w, allowed in self.network.edges(data="allowed")
+                if not allowed
+            ],
+        )
+
+    def set_edge_restriction(self, edge, allowed=True):
+        self.network.edges[edge]["allowed"] = allowed
+
+    def decrease_flow(self, edge):
+        self.network.edges[edge]['flow'] = self.network.edges[edge]["flow"] - 1
+        self.update_edge_attributes(edge)
+
+    def increase_flow(self, edge):
+        self.network.edges[edge]['flow'] = self.network.edges[edge]["flow"] + 1
+        self.update_edge_attributes(edge)
+
+    def update_edge_attributes(self, edge):
+        # Update latency
+        self.network.edges[edge]["latency"] = self.network.edges[edge]["latency_fn"](
+            self.network.edges[edge]["flow"]
+        )
+        self.network.edges[edge]["anticipated_latency"] = self.network.edges[edge]["latency_fn"](
+            self.network.edges[edge]["flow"] + 1)
+
+        # Update tolls
+        new_toll = self.beta * (self.network.edges[edge]['latency'] - self.network.edges[edge]['latency_fn'](0))
+        self.network.edges[edge]['toll'] = self.R * new_toll + (1 - self.R) * self.network.edges[edge]["toll"]
+        new_anticipated_toll = self.beta * (
+                self.network.edges[edge]['anticipated_latency'] - self.network.edges[edge]['latency_fn'](0)
+        )
+        self.network.edges[edge]['anticipated_toll'] = self.R * new_anticipated_toll + (
+                1 - self.R
+        ) * self.network.edges[edge]["anticipated_toll"]
+
+        # Update total costs
+        self.network.edges[edge]['total_cost'] = \
+            self.network.edges[edge]['latency'] + self.network.edges[edge]['toll']
+        self.network.edges[edge]['anticipated_total_cost'] = \
+            self.network.edges[edge]['anticipated_latency'] + self.network.edges[edge]['anticipated_toll']
+
+    def update_network_attributes(self):
+        # Update latencies
+        nx.set_edge_attributes(
+            self.network,
+            {
+                (v, w): attr["latency_fn"](attr["flow"])
+                for v, w, attr in self.network.edges(data=True)
+            },
+            "latency",
+        )
+        nx.set_edge_attributes(
+            self.network,
+            {
+                (v, w): attr["latency_fn"](attr["flow"] + 1)
+                for v, w, attr in self.network.edges(data=True)
+            },
+            "anticipated_latency",
+        )
+
+        # Update tolls
+        new_tolls = {
+            (v, w): self.beta * (attr["latency"] - attr["latency_fn"](0))
+            for v, w, attr in self.network.edges(data=True)
+        }
+        nx.set_edge_attributes(
+            self.network,
+            {
+                (v, w): self.R * new_tolls[(v, w)] + (1 - self.R) * attr["toll"]
+                for v, w, attr in self.network.edges(data=True)
+            },
+            "toll",
+        )
+        new_anticipated_tolls = {
+            (v, w): self.beta * (attr["anticipated_latency"] - attr["latency_fn"](0))
+            for v, w, attr in self.network.edges(data=True)
+        }
+        nx.set_edge_attributes(
+            self.network,
+            {
+                (v, w): self.R * new_anticipated_tolls[(v, w)] + (1 - self.R) * attr["anticipated_toll"]
+                for v, w, attr in self.network.edges(data=True)
+            },
+            "anticipated_toll",
+        )
+
+        # Update total costs
+        nx.set_edge_attributes(
+            self.network,
+            {
+                (v, w): attr["latency"] + attr["toll"]
+                for v, w, attr in self.network.edges(data=True)
+            },
+            "total_cost",
+        )
+        nx.set_edge_attributes(
+            self.network,
+            {
+                (v, w): attr["anticipated_latency"] + attr["anticipated_toll"]
+                for v, w, attr in self.network.edges(data=True)
+            },
+            "anticipated_total_cost",
+        )
+
+    def run_sequentially(self, number_of_steps):
+        assert self._type in [
+            "undefined",
+            "sequentially",
+        ], "Cannot proceed sequentially from a single step model"
+        self._type = "sequentially"
+
+        routes_taken = {
+            car.id: [car.position[0][0], car.position[0][1]]
+            for car in self.cars.values()
+        }
+        for step in range(number_of_steps):
+            if self.verbose:
+                print(f"Step {step}:")
+                print(
+                    f"Positions before step {step}: {[car.position for car in self.cars.values()]}"
+                )
+
+            # Update flow and latencies
+            flow_counter = Counter(car.position[0] for car in self.cars.values())
+            nx.set_edge_attributes(
+                self.network,
+                {edge: flow_counter[edge] for edge in self.network.edges},
+                "flow",
+            )
+            self.update_network_attributes()
+
+            self.step_statistics.append(
+                list(self.routes.values())
+                + list(nx.get_edge_attributes(self.network, "flow").values())
+                + list(nx.get_edge_attributes(self.network, "toll").values())
+                + list(nx.get_edge_attributes(self.network, "latency").values())
+            )
+
+            if self.verbose:
+                print(self)
+
+            # Advance agents
+            for car in self.cars.values():
+                car.position = (car.position[0], min(car.position[1] + car.speed, 1.0))
+
+                if car.position[1] >= 1.0:
+                    self.decrease_flow(car.position[0])
+
+            if self.verbose:
+                print(
+                    f"Positions after step {step}: {[car.position for car in self.cars.values()]}"
+                )
+
+            if self.verbose:
+                print(self)
+
+            # Re-spawn cars which have arrived
+            for car in self.cars.values():
+                if car.position[0][0] == car.target or (
+                    car.position[0][1] == car.target and car.position[1] == 1.0
+                ):
+                    self.car_statistics.append(
+                        {
+                            "step": step,
+                            "car_id": car.id,
+                            "source": car.source,
+                            "target": car.target,
+                            "route": tuple(routes_taken[car.id]),
+                            "travel_time": step - car.created_at_step,
+                        }
+                    )
+
+                    if self.verbose:
+                        print(
+                            f"Car {car.id} reached its target after {step - car.created_at_step} steps."
+                        )
+
+                    # source = np.random.randint(3)
+                    car.reset(car.source, step)
+                    routes_taken[car.id] = [car.source]
+
+            if self.verbose:
+                print(
+                    f"Positions after re-spawning at step {step}: {[car.position for car in self.cars.values()]}"
+                )
+
+            # Let agents make decisions
+            for car in np.random.permutation(
+                [car for car in self.cars.values() if car.position[1] == 1.0]
+            ):
+                self.routes[car.id] = car.act(self.allowed_network)
+                car.position = (car.position[0][1], self.routes[car.id][1]), 0.0
+                self.increase_flow(car.position[0])
+                routes_taken[car.id].append(car.position[0][1])
+
+            if self.verbose:
+                print(self)
+
+        return pd.DataFrame(
+            self.step_statistics,
+            columns=pd.MultiIndex.from_tuples(
+                [("route", car_id) for car_id in self.cars]
+                + [("flow", edge_id) for edge_id in self.network.edges]
+                + [("toll", edge) for edge in self.network.edges]
+                + [("latency", edge_id) for edge_id in self.network.edges]
+            ),
+        ), pd.DataFrame(self.car_statistics)
+
+    def run_single_steps(self, number_of_steps):
+        assert self._type in [
+            "undefined",
+            "single_steps",
+        ], "Cannot proceed in single steps from a sequential model"
+        self._type = "single_steps"
+
+        self.update_network_attributes()
+        for step in (range if self.verbose else trange)(number_of_steps):
+            if self.verbose:
+                print(self)
+
+            for car in np.random.permutation(list(self.cars.values())):
+                for edge in zip(self.routes[car.id], self.routes[car.id][1:]):
+                    self.decrease_flow(edge)
+
+                # Let agents choose a route, given the network with allowed edges only
+                self.routes[car.id] = car.act(self.allowed_network)
+
+                for edge in zip(self.routes[car.id], self.routes[car.id][1:]):
+                    self.increase_flow(edge)
+
+            self.step_statistics.append(
+                list(self.routes.values())
+                + list(nx.get_edge_attributes(self.network, "latency").values())
+                + list(nx.get_edge_attributes(self.network, "toll").values())
+                + [
+                    nx.path_weight(self.network, route, "latency")
+                    for car_id, route in self.routes.items()
+                ]
+            )
+
+            for car in self.cars.values():
+                self.car_statistics.append(
+                    {
+                        "step": step,
+                        "car_id": car.id,
+                        "source": car.source,
+                        "target": car.target,
+                        "route": tuple(self.routes[car.id]),
+                        "travel_time": nx.path_weight(
+                            self.network, self.routes[car.id], "latency"
+                        ),
+                    }
+                )
+
+        return pd.DataFrame(
+            self.step_statistics,
+            columns=pd.MultiIndex.from_tuples(
+                [("route", car_id) for car_id in self.cars]
+                + [("latency", edge) for edge in self.network.edges]
+                + [("toll", edge) for edge in self.network.edges]
+                + [("travel_time", car_id) for car_id in self.cars]
+            ),
+        ), pd.DataFrame(self.car_statistics)
+
+    def __repr__(self):
+        print(f"Current step: {self.current_step}")
+        print(f'{nx.get_edge_attributes(self.network, "flow")=}')
+        print(f'{nx.get_edge_attributes(self.network, "latency")=}')
+        print(f'{nx.get_edge_attributes(self.network, "anticipated_latency")=}')
---
+++ src/traffic/v3/util/network.py
@@ -0,0 +1,167 @@
+# Contains methods to build a traffic network as a networkx graph with additional
+# attributes.
+
+# The edges of the traffic network have the following attributes:
+# (anticipated adds +1 to flow for the subsequent car)
+# - flow: Number of cars currently using the edge
+# - latency_params: Parameters (a, b, c) of the edges' latency function
+# - latency_fn: lambda n: a + b * n ** c
+# - latency: Current latency value (=latency_fn(flow))
+# - toll: Additional cost to use the edge as defined by delta-tolling
+# - total_cost: Sum of latency and toll
+
+import random
+import math
+
+import networkx as nx
+import numpy as np
+
+
+def build_network(network):
+    # Create latency functions from parameters
+    nx.set_edge_attributes(
+        network,
+        {
+            (v, w): lambda u, a=attr["latency_params"][0], b=attr["latency_params"][
+                1
+            ], c=attr["latency_params"][2]: a
+            + b * u**c
+            for v, w, attr in network.edges(data=True)
+        },
+        "latency_fn",
+    )
+
+    # Set initial utilization
+    nx.set_edge_attributes(network, 0, "flow")
+
+    # Set initial latency based on utilization
+    nx.set_edge_attributes(
+        network,
+        {
+            (v, w): attr["latency_fn"](attr["flow"])
+            for v, w, attr in network.edges(data=True)
+        },
+        "latency",
+    )
+
+    # Allow all edges
+    nx.set_edge_attributes(network, True, "allowed")
+
+    # Set tolls to zero
+    nx.set_edge_attributes(network, 0.0, "toll")
+    nx.set_edge_attributes(network, 0.0, "anticipated_toll")
+
+    # Set initial total cost
+    nx.set_edge_attributes(
+        network,
+        {
+            (v, w): attr["latency"] + attr["toll"]
+            for v, w, attr in network.edges(data=True)
+        },
+        "total_cost",
+    )
+
+    return network
+
+
+def create_braess_network(capacity=100):
+    network = nx.DiGraph([(0, 1), (0, 2), (1, 2), (1, 3), (2, 3)])
+
+    nx.set_node_attributes(
+        network, {0: (0, 1), 1: (0.5, 1), 2: (1, 0.5), 3: (1, 0)}, "position"
+    )
+
+    nx.set_edge_attributes(
+        network,
+        {
+            (0, 1): (2, 6 / capacity, 1),
+            (0, 2): (10, 0, 1),
+            (1, 2): (1, 0, 1),
+            (1, 3): (10, 0, 1),
+            (2, 3): (2, 6 / capacity, 1),
+        },
+        "latency_params",
+    )
+
+    return build_network(network)
+
+
+def create_double_braess_network(capacity=100):
+    network = nx.DiGraph(
+        [("A", 0), ("A", 2), (0, 1), (0, 2), (0, "B"), (1, 2), (1, 3), (2, 3), (2, "B")]
+    )
+
+    nx.set_node_attributes(
+        network,
+        {
+            node: (
+                -math.cos(i * 2 * math.pi / (network.number_of_nodes() + 2)),
+                math.sin(i * 2 * math.pi / (network.number_of_nodes() + 2)),
+            )
+            for i, node in enumerate(network.nodes)
+        },
+        "position",
+    )
+
+    nx.set_edge_attributes(
+        network,
+        {
+            ("A", 0): (2, 6 / capacity, 1),
+            ("A", 2): (19, 0, 1),
+            (0, 1): (2, 6 / capacity, 1),
+            (0, 2): (10, 0, 1),
+            (0, "B"): (19, 0, 1),
+            (1, 2): (1, 0, 1),
+            (1, 3): (10, 0, 1),
+            (2, 3): (2, 6 / capacity, 1),
+            (2, "B"): (2, 6 / capacity, 1),
+        },
+        "latency_params",
+    )
+
+    return build_network(network)
+
+
+class LatencyGenerator:
+    pass
+
+
+class ListLatencyGenerator(LatencyGenerator):
+    def __init__(self, possible_params):
+        self.possible_params = possible_params
+
+    def __call__(self):
+        return random.choice(self.possible_params)
+
+
+class UniformLatencyGenerator(LatencyGenerator):
+    def __init__(self, a_min, a_max, b_min, b_max, c_min=1, c_max=1):
+        self.a_min, self.a_max, self.b_min, self.b_max, self.c_min, self.c_max = a_min, a_max, b_min, b_max, c_min, c_max
+
+    def __call__(self):
+        return tuple(np.random.uniform(low=[self.a_min, self.b_min, self.c_min], high=[self.a_max, self.b_max, self.c_max]))
+
+
+def create_random_grid_network(number_of_rows, number_of_columns, latency_generator):
+    network = nx.grid_2d_graph(
+        number_of_rows, number_of_columns, create_using=nx.DiGraph
+    )
+
+    nx.set_node_attributes(
+        network,
+        {
+            (i, j): (j / (number_of_columns - 1), 1 - (i / (number_of_rows - 1)))
+            for i, j in network.nodes
+        },
+        "position",
+    )
+    nx.set_edge_attributes(
+        network,
+        {
+            edge: latency_generator()
+            for edge in network.edges
+        },
+        "latency_params",
+    )
+
+    return build_network(network)
