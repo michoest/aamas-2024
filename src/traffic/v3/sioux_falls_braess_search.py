f4c06eecba0f64422b8ca0008efc4b41773da7f0
Tim Grams <t.grams339@gmail.com>
2023-08-25 15:46:38 +0200

    sioux falls update

--- src/traffic/v3/sioux_falls_braess_search.py
+++ src/traffic/v3/sioux_falls_braess_search.py
@@ -7,9 +7,9 @@ from src.traffic.v3.util.network import create_sioux_falls_network, create_sioux
 from src.traffic.v3.util.utils import save_or_extend_dataframe
 
 
-network_path = 'data/SiouxFalls_net.tntp'
-costs_path = 'data/sf1_c.txt'
-cars_path = 'data/sf1_dem.txt'
+network_path = 'src/traffic/v3/data/SiouxFalls_net.tntp'
+costs_path = 'src/traffic/v3/data/sf1_c.txt'
+cars_path = 'src/traffic/v3/data/sf1_dem.txt'
 
 
 def single_run(steps=2000, edge=None):
@@ -26,16 +26,25 @@ def single_run(steps=2000, edge=None):
 
 def find_braess(steps):
     # Initialize dataframe with unrestricted performance
-    combined_results = single_run(steps).groupby(['source', 'target'])['travel_time'].mean().reset_index()
+    car_statistics = single_run(steps)
+    combined_results = car_statistics[car_statistics['step'] >= steps / 2].groupby(
+        ['source', 'target'])['travel_time'].mean().reset_index()
     combined_results.columns = ['source', 'target', 'unrestricted']
 
     # Block each edge and measure effect
-    for edge in create_sioux_falls_network(network_path, costs_path).edges:
-        combined_results = pd.merge(combined_results,
-                                    single_run(steps, edge).groupby(['source', 'target'])[
-                                        'travel_time'].mean().reset_index(),
-                                    how='outer', left_on=['source', 'target'], right_on=['source', 'target'])
-        combined_results.columns = list(combined_results.columns)[:-1] + [edge]
+    for index, edge in enumerate(create_sioux_falls_network(network_path, costs_path).edges):
+        if edge[0] != edge[1]:
+            print(f'Blocking edge {edge} ({index})')
+            car_statistics = single_run(steps, edge)
+            combined_results = pd.merge(combined_results,
+                                        car_statistics[car_statistics['step'] >= steps / 2]
+                                        .groupby(['source', 'target'])['travel_time'].mean().reset_index(),
+                                        how='outer', left_on=['source', 'target'], right_on=['source', 'target'])
+            combined_results.columns = list(combined_results.columns)[:-1] + [edge]
+        else:
+            print(f'Skipping self-edge {edge}')
+
+        print(combined_results.head())
 
     return combined_results
 
@@ -45,6 +54,6 @@ if __name__ == "__main__":
     parser.add_argument('--steps', type=int, required=True)
     parser.add_argument('--name', type=str, required=False, default='results')
     args = parser.parse_args()
-    print(f'Starting braess search with {args.samples} with {args.steps} for {args.seconds} seconds!!')
+    print(f'Starting braess search with {args.steps} steps!!')
 
     save_or_extend_dataframe(find_braess(args.steps), f'{args.name}.pkl')
