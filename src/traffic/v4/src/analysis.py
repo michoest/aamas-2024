5401c47f7124aa93d584073d0f4a06aa42fe8d79
Michael Oesterle <michaeloesterle01@gmail.com>
2023-08-31 08:40:47 +0200

    Add drawing

--- src/traffic/v4/src/analysis.py
+++ src/traffic/v4/src/analysis.py
@@ -1,6 +1,8 @@
 # External modules
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import networkx as nx
 
 
 def compute_regression(data):
@@ -22,3 +24,19 @@ def analyze_fairness(car_stats, cutoff=0):
     slope, intercept, error = compute_regression(data)
 
     return {"slope": slope, "error": error}
+
+
+def draw_network(network, ax=None):
+    if ax is None:
+        _, ax = plt.subplots()
+
+    ax.set_title("Network")
+    ax.figure.set_size_inches(5, 5)
+    nx.draw(
+        network,
+        ax=ax,
+        pos=nx.get_node_attributes(network, "position"),
+        with_labels=True,
+        font_size=8,
+        edgelist=[(v, w) for v, w in network.edges if v != w]
+    )
