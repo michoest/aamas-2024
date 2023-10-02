fb19b37b3ee484d9472071ccde4310ec09915690
Tim Grams <t.grams339@gmail.com>
2023-10-02 16:11:11 +0200

    deleted deprecated files and cleared outputs

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# aamas-2024
+# RAISE the Bar: Restriction of Action Spaces for Improved Social Welfare and Equity in Traffic Management 
+Code package for the reproduction of experimental results
+
+## Structure
+### `/experiments`
+Contains the experiments `/braess` (Generalized Braess graphs) and `/gnp` (Erdös-Renyi graphs) for the effect of restriction-based and reward-based governance of multi-agent systems in the domain of traffic management.
+
+### `/src`
+Contains the definition of the traffic model and the agents (`environment.py`), functions for the analysis and visualization of the results (`analysis.py`) and utility functions (`util.py`).
+
+## Installation
+To install the required dependencies, run
+```
+$ pip install raise-the-bar
+```
+
+## Execution
+The notebooks in the `/experiments` folder can be simply run from start to end to reproduce the results reported in the paper. Using different seeds will give slightly different results, showing that the claims are robust with respect to randomization.
+
+Any plots are generated in the respective experiment folders.
--- src/environment.py
+++ src/environment.py
@@ -1,5 +1,7 @@
+# Internal modules
 from collections import Counter
 
+# External modules
 import networkx as nx
 import numpy as np
 import pandas as pd
@@ -272,9 +274,6 @@ class TrafficModel:
                 + list(nx.get_edge_attributes(self.network, "toll").values())
             )
 
-            # if self.verbose:
-            #     print(self)
-
             # Advance agents
             for car in self.cars.values():
                 car.position = (car.position[0], min(car.position[1] + car.speed, 1.0))
@@ -286,9 +285,6 @@ class TrafficModel:
                 print(
                     f"Positions after step {step}: {[car.position for car in self.cars.values()]}"
                 )
-
-            # if self.verbose:
-            #     print(self)
 
             # Re-spawn cars which have arrived
             for car in self.cars.values():
@@ -333,9 +329,6 @@ class TrafficModel:
                 car.position = (car.position[0][1], self.routes[car.id][1]), 0.0
                 self.increase_flow(car.position[0])
                 routes_taken[car.id].append(car.position[0][1])
-
-            # if self.verbose:
-            #     print(self)
 
         return pd.DataFrame(
             self.step_statistics,
@@ -411,102 +404,175 @@ class TrafficModel:
         print(f'{nx.get_edge_attributes(self.network, "latency")=}')
         print(f'{nx.get_edge_attributes(self.network, "anticipated_latency")=}')
 
-    def run_sequentially_with_phases(self, phases, *, show_progress=True):
-        assert self._type in [
-            "undefined",
-            "sequentially",
-        ], "Cannot proceed sequentially from a single step model"
-        self._type = "sequentially"
+class LatencyGenerator:
+    def __init__(self, *, seed=42) -> None:
+        self.rng = np.random.default_rng(seed)
 
-        routes_taken = {
-            car.id: [car.position[0][0], car.position[0][1]]
-            for car in self.cars.values()
-        }
 
-        cumulated_step = 0
-        for car_counts, number_of_steps in phases:
-            print(f'Running demand {car_counts} for {number_of_steps} steps...')
-            demand = [item for s_t, count in car_counts.items() for item in [s_t] * count]
-            self.rng.shuffle(demand)
-            demand = dict(enumerate(demand))
+class ListLatencyGenerator(LatencyGenerator):
+    def __init__(self, possible_params, *, seed=42):
+        super().__init__(seed=seed)
+        self.possible_params = possible_params
 
-            for phase_step in range(number_of_steps):
-                # Update flow and latencies
-                flow_counter = Counter(car.position[0] for car in self.cars.values())
-                nx.set_edge_attributes(
-                    self.network,
-                    {edge: flow_counter[edge] for edge in self.network.edges},
-                    "flow",
+    def __call__(self):
+        return self.rng.choice(self.possible_params)
+
+
+class UniformLatencyGenerator(LatencyGenerator):
+    def __init__(
+        self,
+        a_min,
+        a_max,
+        b_min,
+        b_max,
+        c_min=1,
+        c_max=1,
+        d_min=1,
+        d_max=1,
+        *,
+        integer=False,
+        seed=42,
+    ):
+        super().__init__(seed=seed)
+        (
+            self.a_min,
+            self.a_max,
+            self.b_min,
+            self.b_max,
+            self.c_min,
+            self.c_max,
+            self.d_min,
+            self.d_max,
+        ) = (a_min, a_max, b_min, b_max, c_min, c_max, d_min, d_max)
+        self.integer = integer
+
+    def __call__(self):
+        if self.integer:
+            return tuple(
+                self.rng.randint(
+                    low=[self.a_min, self.b_min, self.c_min, self.d_min],
+                    high=[
+                        self.a_max + 1,
+                        self.b_max + 1,
+                        self.c_max + 1,
+                        self.d_max + 1,
+                    ],
                 )
-                self.update_network_attributes()
-
-                self.step_statistics.append(
-                    list(self.routes.values())
-                    + list(nx.get_edge_attributes(self.network, "flow").values())
-                    + list(nx.get_edge_attributes(self.network, "latency").values())
-                    + list(nx.get_edge_attributes(self.network, "toll").values())
+            )
+        else:
+            return tuple(
+                self.rng.uniform(
+                    low=[self.a_min, self.b_min, self.c_min, self.d_min],
+                    high=[self.a_max, self.b_max, self.c_max, self.d_max],
                 )
+            )
 
-                # Advance agents
-                for car in self.cars.values():
-                    car.position = (car.position[0], min(car.position[1] + car.speed, 1.0))
 
-                    if car.position[1] >= 1.0:
-                        self.decrease_flow(car.position[0])
+class OneXLatencyGenerator(LatencyGenerator):
+    """Selects randomly one of two latency functions:
+    - l(n) = 1 + n / c
+    - l(n) = 2
+    Using these values, it is always beneficial to choose a variable-latency edge over
+    a constant-latency edge (as in the original Braess Paradox).
+    """
 
-                # Re-spawn cars which have arrived
-                for car in self.cars.values():
-                    if car.position[0][0] == car.target or (
-                        car.position[0][1] == car.target and car.position[1] == 1.0
-                    ):
-                        self.car_statistics.append(
-                            {
-                                "step": cumulated_step,
-                                "car_id": car.id,
-                                "value_of_time": car.value_of_time,
-                                "value_of_money": car.value_of_money,
-                                "source": car.source,
-                                "target": car.target,
-                                "route": tuple(routes_taken[car.id]),
-                                "travel_time": cumulated_step - car.created_at_step,
-                                "toll": car.toll,
-                                "total_cost": (cumulated_step - car.created_at_step)
-                                * car.value_of_time
-                                + car.toll * car.value_of_money,
-                            }
-                        )
+    def __init__(self, *, q=0.5, capacity=100, seed=42):
+        super().__init__(seed=seed)
+        self.q = q
+        self.capacity = capacity
 
-                        if self.verbose:
-                            print(
-                                f"Car {car.id} reached its target after {cumulated_step - car.created_at_step} steps."
-                            )
+    def __call__(self):
+        return self.rng.choice(
+            [(1, 1, self.capacity, 1), (2, 0, 1, 1)], p=[self.q, 1.0 - self.q]
+        )
 
-                        car.reset(*demand[car.id], cumulated_step)
-                        routes_taken[car.id] = [car.source]
 
-                if self.verbose:
-                    print(
-                        f"Positions after re-spawning at step {cumulated_step}: {[car.position for car in self.cars.values()]}"
-                    )
+def update_latency_functions(network):
+    nx.set_edge_attributes(
+        network,
+        {
+            (v, w): lambda n, params=attr["latency_params"]: params[0]
+            + params[1] * (n / params[2]) ** params[3]
+            for v, w, attr in network.edges(data=True)
+        },
+        "latency_fn",
+    )
 
-                # Let agents make decisions
-                for car in self.rng.permutation(
-                    [car for car in self.cars.values() if car.position[1] == 1.0]
-                ):
-                    self.routes[car.id] = car.act(self.allowed_network)
 
-                    car.position = (car.position[0][1], self.routes[car.id][1]), 0.0
-                    self.increase_flow(car.position[0])
-                    routes_taken[car.id].append(car.position[0][1])
+def build_network(network):
+    # Add self-loops
+    network.add_edges_from(
+        [(v, v) for v in network.nodes if (v, v) not in network],
+        latency_params=(1, 0, 1, 1),
+    )
 
-                cumulated_step += 1
+    # Create latency functions from parameters: l(n) = a + b * (n / c) ** d
+    update_latency_functions(network)
 
-        return pd.DataFrame(
-            self.step_statistics,
-            columns=pd.MultiIndex.from_tuples(
-                [("route", car_id) for car_id in self.cars]
-                + [("flow", edge_id) for edge_id in self.network.edges]
-                + [("latency", edge_id) for edge_id in self.network.edges]
-                + [("toll", edge_id) for edge_id in self.network.edges]
-            ),
-        ), pd.DataFrame(self.car_statistics)
+    # Set initial utilization
+    nx.set_edge_attributes(network, 0, "flow")
+
+    # Set initial latency based on utilization
+    nx.set_edge_attributes(
+        network,
+        {
+            (v, w): attr["latency_fn"](attr["flow"])
+            for v, w, attr in network.edges(data=True)
+        },
+        "latency",
+    )
+
+    # Allow all edges
+    nx.set_edge_attributes(network, True, "allowed")
+
+    # Set tolls to zero
+    nx.set_edge_attributes(network, 0.0, "toll")
+    nx.set_edge_attributes(network, 0.0, "anticipated_toll")
+
+    return network
+
+
+def create_cars(network, car_counts, seed=42):
+    rng = np.random.default_rng(seed)
+    cars = {}  # Dictionary of final cars
+    # Dictionary storing counts of cars on each edge for the different goals
+    car_distribution = {edge: [0] * len(car_counts) for edge in network.edges}
+    # Iterate through (start, goal)-combinations
+    for goal_index, ((s, t), count) in enumerate(car_counts.items()):
+        # and get their feasible edges and corresponding latencies
+        feasible_edges = [
+            edge
+            for edge in network.edges
+            if nx.has_path(network, s, edge[0]) and nx.has_path(network, edge[1], t) and edge[0] != t
+        ]
+        latencies = [network.edges[edge]["latency_fn"](0) for edge in feasible_edges]
+
+        # Randomly distribute the cars on the edges weighted by the latency
+        edge_cars = rng.choice(
+            range(len(feasible_edges)), count, p=np.array(latencies) / sum(latencies)
+        )
+
+        # Collect the counts for each goal in a separate dictionary
+        if len(edge_cars) > 0:
+            for car in edge_cars:
+                car_distribution[feasible_edges[car]][goal_index] += 1
+
+    # Iterate through all edges with the counts for all goals
+    for edge, remaining_cars in car_distribution.items():
+        cars_on_edge = 0
+        # and distribute them in a random order on the edge
+        while np.sum(remaining_cars) != 0:
+            choice_of_goal = rng.choice(
+                list(range(len(remaining_cars))),
+                p=remaining_cars / np.sum(remaining_cars),
+            )
+            cars_on_edge += 1
+            cars[len(cars)] = Car(
+                len(cars),
+                list(car_counts.keys())[choice_of_goal][0],
+                list(car_counts.keys())[choice_of_goal][1],
+                speed=1 / network.edges[edge]["latency_fn"](cars_on_edge),
+                position=(edge, 0.0),
+            )
+            remaining_cars[choice_of_goal] -= 1
+    return cars
